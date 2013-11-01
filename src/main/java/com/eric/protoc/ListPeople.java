5a0813b14d7c7a71dd7e7164a631fcb6a8adb965
yougang <gangyou@gmail.com>
2013-11-01 16:35:00 +0800

    Google Protocol Buffer

---
+++ src/main/java/com/eric/protoc/ListPeople.java
@@ -0,0 +1,45 @@
+package com.eric.protoc;
+
+import java.io.FileInputStream;
+
+import com.eric.protoc.AddressBookProtos.AddressBook;
+import com.eric.protoc.AddressBookProtos.Person;
+
+public class ListPeople {
+    static void print(AddressBook addressBook) {
+        for (Person person : addressBook.getPersonList()) {
+            System.out.println("Person ID: " + person.getId());
+            System.out.println("  Name: " + person.getName());
+            if (person.hasEmail()) {
+                System.out.println("  E-mail address: " + person.getEmail());
+            }
+
+            for (Person.PhoneNumber phoneNumber : person.getPhoneList()) {
+                switch (phoneNumber.getType()) {
+                case MOBILE:
+                    System.out.print("  Mobile phone #: ");
+                    break;
+                case HOME:
+                    System.out.print("  Home phone #: ");
+                    break;
+                case WORK:
+                    System.out.print("  Work phone #: ");
+                    break;
+                }
+                System.out.println(phoneNumber.getNumber());
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1){
+            System.err.println("Usage: ListPeople ADDRESS_BOOK_FILE");
+            System.exit(-1);
+        }
+        
+        AddressBook addressBook = 
+                   AddressBook.parseFrom(new FileInputStream(args[0]));
+        
+        print(addressBook);
+    }
+}
